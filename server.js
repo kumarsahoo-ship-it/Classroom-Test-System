45bbe286c1149bc55b19e327d668951f707b8876
KumarSahoo <sahoodhirenkumar10@gmail.com>
2026-04-16 20:32:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,11 +21,6 @@ app.get("/test", (req, res) => {
 });
 
 
-const express = require("express");
-const app = express();
-
-app.use(express.json());
-
 let users = [];
 
 app.get("/", (req, res) => {
